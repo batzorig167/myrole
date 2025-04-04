fd8edec6203c3330db918affe7e46bb6f492699d
Bayra <tsbanzai74@gmail.com>
2025-04-04 23:58:28 +0800

    homepage HomePage

--- src/pages/components/LandingPage.jsx
+++ src/pages/components/LandingPage.jsx
@@ -14,7 +14,7 @@ export default function LandingPage() {
     function closeHandle(){
         setSelectItem(null)
     }
-    return <div className="bg-[#e3f6f5] max-auto-screen text-black">  
+    return <div className="bg-[#e3f6f5] max-auto-screen text-black w-125">  
             <div className="flex flex-col bg-[#fff]">
                 <div className="flex justify-center gap-70">
                     <button><Link href="#"><img src="https://bcassetcdn.com/social/tqtwz2wdy6/preview.png" alt="" className="h-20 rounded-md"/></Link></button>
@@ -52,15 +52,15 @@ export default function LandingPage() {
                         {/* <a href="#" onClick={handleBtn} className="border px-6 py-2 rounded-full text-xl rounded-full text-xl h-12 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-[#fff] bg-[#f9593a] hover:bg-blue-500">Test өгөх</a> */}
                         {
             selectItem && (
-                <div className="fixed top-0 left-0 w-[100vw] h-[100vh] ">
+                <div className="fixed top-0 left-0 w-[100wh] pl-10 h-[100vh] justify-center items-center ">
                     <div className="w-[100vw] h-[100vh] relative flex justify-center items-center">
                         <button onClick={closeHandle} className="w-[100vw]  h-[100vh] absolute-50 top-0 left-0" >
                             lol
                         </button>
-                        <div className="bg-slate-600 p-3 rounded-xl absolute z-[1] flex flex-col w-180 h-110">
-                            <div className="flex justify-between text-white">
-                                <h1 className="text-3xl text-center pt-3 capitalize pl-40">Test өгөх боломжтой сэдвүүд</h1>
-                                <button type="button" onClick={closeHandle} class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <div className="bg-slate-600 p-3 rounded-xl absolute z-[1] flex flex-col w-100 h-120">
+                            <div className="flex justify-between text-white gap-3">
+                                <h1 className="text-2xl text-center pt-3 pl-10">Test өгөх боломжтой сэдвүүд</h1>
+                                <button type="button" onClick={closeHandle} class="bg-white rounded-md px-1 h-10 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                 <span class="sr-only">Close menu</span>
                                 <svg class="h-6 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
