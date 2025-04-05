b25ccbd6f3fc767893138febfe268d9200f1bb0e
bertsetseg54 <tuvshinjargalbertsetseg54@gmail.com>
2025-04-05 11:58:02 +0800

    challenge nemelt design

--- src/pages/components/Challenge.jsx
+++ src/pages/components/Challenge.jsx
@@ -9,16 +9,16 @@ export default function Challenge({props})
     function closeHandle(){
         setSelectItem(null)
     }
-     return <div className="h-[100vh] flex flex-col justify-center items-center gap-2">
-        <h1 className="text-xl flex justify-center">{"Таний сэтгэл зүйн түвшин:"+result}</h1>
-        <h1 className="text-xl flex justify-center">Танд санал болгох чалленж даалгаврууд</h1>
+     return <div className="h-[100vh] flex flex-col justify-center items-center gap-2 bg-[#e3f6f5]">
+        <h1 className="text-xl flex justify-center text-[#272343]">{"Таний сэтгэл зүйн түвшин:"+result}</h1>
+        <h1 className="text-xl flex justify-center text-[#272343]">Танд санал болгох чалленж даалгаврууд</h1>
 
-     <div className="flex flex-col gap-5">
+     <div className="flex flex-col gap-5 items-center">
          {
              props.map((data,index)=>{
                  return <div key={index}>
                     
-                    <button className="rounded-full text-center text-xl w-150 py-2 h-16 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-[#fff] border text-black rounded-full hover:bg-[#f9593a] bg-blue-400" onClick={handleBtn}>{data.name}</button>
+                    <button className="text-center border border-[#272343] text-xl px-[60px] py-[10px] transition delay-150 duration-300 ease-in-out rounded-full text-[#272343] bg-[#fffffe] max-w-[700px]" onClick={handleBtn}>{data.name}</button>
                      {/* <p><strong>Чалленжийн нэр: </strong>{data.name}</p>
                      <p><strong>Даалгавар: </strong>{data.daalgavar}</p>
                          {
@@ -43,11 +43,11 @@ export default function Challenge({props})
                         <button onClick={closeHandle} className="w-[100vw]  h-[100vh] absolute-50 top-0 left-0" >
                             lol
                         </button>
-                        <div className="bg-gray-400 py-2 px-5 rounded-xl absolute z-[1] flex flex-col w-180 ">
+                        <div className="bg-[#272343] py-2 px-5 rounded-xl absolute z-[1] flex flex-col  text-[#fffffe]">
                             <div className="flex flex-col justify-evenly gap-3">
                                 <div className="flex justify-between">
                                 <p className="text-xl"><strong>Чалленжийн нэр: </strong>{data.name}</p>
-                                    <button type="button" onClick={closeHandle} class="bg-gray-400 rounded-md inline-flex items-center justify-center text-black hover:text-gray-500 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-indigo-500">
+                                    <button type="button" onClick={closeHandle} class="bg-[#272343] rounded-md inline-flex items-center justify-center text-[#fffffe] focus:outline-none focus:ring-1 focus:ring-inset focus:ring-indigo-500">
                                     <span class="sr-only">Close</span>
                                     <svg class="h-6 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -70,9 +70,9 @@ export default function Challenge({props})
                                     </>
                                 )
                                 }
-                                <p><strong>Зорилго: </strong>{data.zorilgo}</p>
+                                <p className="text-xl"><strong>Зорилго:</strong>{data.zorilgo}</p>
                             </div>
-                            <button onClick={closeHandle} className="text-right pt-13 pr-5 text-black">Хаах</button>
+                            <button onClick={closeHandle} className="text-right pt-13 pr-5 text-[#bae8e8]">Хаах</button>
                         </div>
                     </div>
                 </div>
