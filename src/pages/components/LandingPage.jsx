b81bbe966380547ced8a5b4e08fcf4a7695c2728
Imac-8 <imac-8@Imac-8s-iMac.local>
2025-04-06 18:54:00 +0800

    LandingPage

--- src/pages/components/LandingPage.jsx
+++ src/pages/components/LandingPage.jsx
@@ -16,7 +16,7 @@ export default function LandingPage() {
     }
     return <div className="bg-[#e3f6f5] m-auto text-black">  
             <div className="flex flex-col bg-[#fff] max-w-[600px] m-auto md:max-w-[1000px]">
-                <div className="flex justify-between w-[100%]">
+                <div className="flex justify-between w-[100%] ">
                     <button><Link href="#"><img src="https://bcassetcdn.com/social/tqtwz2wdy6/preview.png" alt="" className="h-20 rounded-md"/></Link></button>
                     <div className="flex gap-10 px-5 flex-col justify-center">
                         {/* <a href="login" className="border rounded-md px-4 h-10 flex items-center">Бүртгүүлэх</a> */}
@@ -30,7 +30,7 @@ export default function LandingPage() {
                             <ul className="menu menu-sm dropdown-content bg-white text-black rounded-box z-1 mt-3 w-52 p-2 text-base">
 
                                 <li>
-                                    <a className="justify-between text-white">Profile</a>
+                                    <a className="justify-between text-black">Profile</a>
                                 </li>
                                     <li><a>Settings</a></li>
                                     <li><a href="/homepage">Гарах</a></li>
@@ -49,8 +49,11 @@ export default function LandingPage() {
                         <button onClick={handleBtn} className="px-20 py-2 rounded-full text-xl text-[#272343] bg-[#ffd803] font-semibold">Тест өгөх</button>
                         {
                             selectItem && (
-                                <div className="fixed top-0 left-0 pl-10 justify-center items-center">
-                                    <div className="relative flex justify-center items-center">
+                                <div className="fixed top-0 left-0 pl-10 justify-center items-center w-[100vw] h-[100vh]">
+                                    <div className="relative flex justify-center items-center w-[100vw] h-[100vh]">
+                                            <button onClick={closeHandle} className="w-[100vw]  h-[100vh] absolute-50 top-0 left-0" >
+                                                1
+                                            </button>
                                         <div className="bg-slate-600 p-3 rounded-xl absolute z-[1] flex flex-col">
                                             <div className="flex justify-between text-white gap-3">
                                                 <h1 className="text-2xl text-center pt-3 pl-10">Test өгөх боломжтой сэдвүүд</h1>
@@ -61,7 +64,7 @@ export default function LandingPage() {
                                                 </svg>
                                                 </button>
                                             </div>
-                                            <div className="flex flex-col gap-5 justify-center pt-15 text-white border">
+                                            <div className="flex flex-col gap-5 justify-center pt-15 text-white ">
                                                 <a href="/taketest" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black  hover:bg-stone-50">Сэтгэл гутрал</a>
                                                 <a href="" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black hover:bg-stone-50">Түгшүүр</a>
                                                 <a href="" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black hover:bg-stone-50">Стресс</a>
