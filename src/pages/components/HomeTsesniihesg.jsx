25b06f1513074cb0a79c63450809def66705e142
bertsetseg54 <tuvshinjargalbertsetseg54@gmail.com>
2025-04-07 18:51:30 +0800

    bertsetsegin style

--- src/pages/components/HomeTsesniihesg.jsx
+++ src/pages/components/HomeTsesniihesg.jsx
@@ -1,53 +1,75 @@
 import Link from "next/link";
 import { useState } from "react";
+import { useCategory } from "../Context/CategoryContext";
+import { useRouter } from "next/router";
 
 export default function HomeTsesniihesg() {
     const [selectItem,setSelectItem] = useState(null);
+    const {setCatindex,setCtegory} = useCategory();
+    const router = useRouter();
     function handleBtn(){
         setSelectItem(2);
     }
     function closeHandle(){
         setSelectItem(null)
     }
-    return <div className="flex flex-col pb-5 gap-2">
-        <div className="flex md:flex-row flex-col items-center gap-5 p-5">
-            <img src="https://thumbs.dreamstime.com/b/friendly-male-doctor-cartoon-character-floral-plant-decoration-346249083.jpg" alt="" className="h-60 flex md:h-80 pt-8"/>
-                <div className="flex flex-col gap-4 pt-5 ">
-                    <h2 className=" text-justify text-xl  flex items-center">Энэхүү сайт нь стресс, сэтгэл гутрал, түгшүүр, өөртөө итгэх итгэл гэсэн дөрвөн цогц дэд сэдвээс бүрдсэн сайт юм.Сайтын үр дагавар нь өөртөө итгэх итгэлийг нэмэгдүүлэх, стресс, түгшүүрээс хол байх, сэтгэл гутралыг эмчлэх, өөрийн буруу зүйлээ ойлгуулахад чиглэнэ.</h2>
-                {/* <a href="#" className="rounded-full text-center text-xl h-12 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-[#fff] border  py-2 rounded-full bg-[#f9593a] hover:bg-blue-500" onClick={handleBtn}>Test өгөх</a> */}
-                </div>
+    let changeTest = (props)=>{
+        // alert(props)
+        if(props == "Өөртөө итгэх итгэл"){
+            setCatindex(3);
+        }       
+        if(props == "Стресс"){
+            setCatindex(2);
+        }
+        if(props == "Түгшүүр"){
+            setCatindex(1);
+        }
+        if(props == "Сэтгэл гутрал"){
+            setCatindex(0);
+        }
+        router.push("/taketest")
+    }
+    return <div className="flex flex-col pb-5 bg-[white] md:rounded-t-2xl p-[30px] md:flex-row items-center gap-5">
+        <div className="flex justify-center md:w-auto">
+            <img src="https://thumbs.dreamstime.com/b/friendly-male-doctor-cartoon-character-floral-plant-decoration-346249083.jpg" alt="" className="h-60 md:w-full  md:h-auto"/>
         </div>
-        <div className="flex justify-center">
-            <button className="rounded-full text-center text-xl h-12 w-100 text-[#fff] border  py-2 rounded-full bg-[#f9593a] hover:bg-blue-700" onClick={handleBtn}>Test өгөх</button>
+        <div className="flex flex-col pt-5"> 
+            <div className=" flex items-center">
+                 <h2 className="text-justify text-xl">Энэхүү сайт нь стресс, сэтгэл гутрал, түгшүүр, өөртөө итгэх итгэл гэсэн дөрвөн цогц дэд сэдвээс бүрдсэн сайт юм. Сайтын үр дагавар нь өөртөө итгэх итгэлийг нэмэгдүүлэх, стресс, түгшүүрээс хол байх, сэтгэл гутралыг эмчлэх, өөрийн буруу зүйлээ ойлгуулахад чиглэнэ.</h2>
+                {/* <a href="#" className="rounded-full text-center text-xl h-12 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-[#fff] border  py-2 rounded-full bg-[#f9593a] hover:bg-blue-500" onClick={handleBtn}>Test өгөх</a> */}
+            </div>
+            <div className="flex justify-center md:justify-end md:mr-[60px]">
+                <button className="rounded-full text-center text-lg text-[#fff] border px-[20px] py-1 bg-[#1F98DC]" onClick={handleBtn}>Тест өгөх </button>
+            </div>
         </div>
         {
-            selectItem && (
-                <div className="fixed top-0 left-0 pl-10  w-[100vw] h-[100vh] ">
-                    <div className="w-[100vw] h-[100vh] relative flex justify-center items-center">
-                        <button onClick={closeHandle} className="w-[100vw]  h-[100vh] absolute-50 top-0 left-0" >
-                            lol
+           selectItem && (
+            <div className="fixed top-0 left-0 justify-center items-center w-[100vw] h-[100vh]">
+                <div className="relative flex justify-center items-center w-[100vw] h-[100vh]">
+                        <button onClick={closeHandle} className="w-[100vw]  h-[100vh] absolute-50 bg-[black] opacity-50 inset-0" >
+                            1
                         </button>
-                        <div className="bg-slate-600 p-3 rounded-xl absolute z-[1] flex flex-col w-100 h-120">
-                            <div className="flex justify-between text-white">
-                                <h1 className="text-2xl text-center pt-3 pl-10">Test өгөх боломжтой сэдвүүд</h1>
-                                <button type="button" onClick={closeHandle} class="bg-white rounded-md p-1 h-10 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                <span class="sr-only">Close menu</span>
-                                <svg class="h-6 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                                </button>
-                            </div>
-                            <div className="flex flex-col gap-5 justify-center pt-15 text-white">
-                                <a href="/taketest" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2  hover:text-black  hover:bg-stone-50">Сэтгэл гутрал</a>
-                                <a href="/Tugshuur" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black hover:bg-stone-50">Түгшүүр</a>
-                                <a href="/Stress" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2  hover:text-black hover:bg-stone-50">Стресс</a>
-                                <a href="/Itgel" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2  hover:text-black hover:bg-stone-50">Өөртөө итгэх итгэл</a>
-                            </div>
-                            <button onClick={closeHandle} className="text-right pt-13 pr-5 text-white">Хаах</button>
+                    <div className="bg-[white] p-3 rounded-xl absolute z-[1] flex flex-col">
+                        <div className="flex justify-between text-white gap-3">
+                            <h1 className="text-2xl text-center pt-2 pl-10 text-[#272343]">Тест өгөх боломжтой сэдвүүд</h1>
+                            <button type="button" onClick={closeHandle} class="rounded-md px-[5px] pl-[] h-10 inline-flex items-center justify-center text-gray-500 pt-3">
+                            <span class="sr-only">Close menu</span>
+                            <svg class="h-6 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                            </button>
                         </div>
+                        <div className="flex flex-col gap-5 justify-center pt-10 px-[20px]">
+                            <button onClick={()=>changeTest("Сэтгэл гутрал")} href="" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Сэтгэл гутрал</button>
+                            <button onClick={()=>changeTest("Түгшүүр")} href="" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Түгшүүр</button>
+                            <button onClick={()=>changeTest("Стресс")} href="" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Стресс</button>
+                            <button onClick={()=>changeTest("Өөртөө итгэх итгэл")} href="" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Өөртөө итгэх итгэл</button>
+                        </div>
+                        <button onClick={closeHandle} className="text-right pt-[20px] px-[10px] text-[#2d334a]">Хаах</button>
                     </div>
                 </div>
-            )
+            </div>
+        )
         }
     </div>
 }
--- src/pages/components/LandingPage.jsx
+++ src/pages/components/LandingPage.jsx
@@ -34,10 +34,10 @@ export default function LandingPage() {
         }
         router.push("/taketest")
     }
-    return <div className="bg-[#e3f6f5] m-auto text-black">  
-            <div className="flex flex-col bg-[#fff] max-w-[600px] m-auto md:max-w-[1000px]">
-                <div className="flex justify-between w-[100%] ">
-                    <button><Link href="#"><img src="https://bcassetcdn.com/social/tqtwz2wdy6/preview.png" alt="" className="h-20 rounded-md"/></Link></button>
+    return <div className="bg-[white] m-auto text-black">  
+            <div className="flex flex-col max-w-[600px] m-auto md:max-w-[1000px]">
+                <div className="flex justify-between w-[100%] md:rounded-b-2xl bg-[#fff] pl-[30px]"> 
+                    <button><Link href="#"><img src="https://dynamic.design.com/preview/logodraft/e2387455-d3c5-4701-bd2c-8cadb44690e1/image/large.png" alt="" className="h-20 rounded-md"/></Link></button>
                     <div className="flex gap-10 px-5 flex-col justify-center">
                         {/* <a href="login" className="border rounded-md px-4 h-10 flex items-center">Бүртгүүлэх</a> */}
                         <div className="dropdown dropdown-end">
@@ -62,35 +62,35 @@ export default function LandingPage() {
                     <div className="flex justify-center">
                         <div className="items-center flex flex-col justify-center rounded-[18px] bg-[#fffffe] w-[90%] p-[18px]">
                             <p className="flex justify-center text-3xl pb-3 text-[#272343] font-semibold md:text-4xl">Сэтгэл судлалын сайт</p>
-                            <p className="flex justify-center text-xl italic text-justify text-[#2d334a] md:text-2xl">Энэхүү сэтгэл зүйн сайт нь хүмүүсийн сэтгэл зүйн эрүүл мэндийг дэмжих, зөвлөгөө өгөх, өөрийгөө хөгжүүлэх боломж олгох зорилготой. Энэ нь сэтгэлзүйн боловсрол түгээх, мэргэжлийн зөвлөгөө хүргэх, стресс, түгшүүрийг даван туулахад туслах платформ болно.</p>
+                            <p className="flex justify-center text-lg italic text-justify text-[#2d334a] md:text-xl">Энэхүү сэтгэл зүйн сайт нь хүмүүсийн сэтгэл зүйн эрүүл мэндийг дэмжих, зөвлөгөө өгөх, өөрийгөө хөгжүүлэх боломж олгох зорилготой. Энэ нь сэтгэлзүйн боловсрол түгээх, мэргэжлийн зөвлөгөө хүргэх, стресс, түгшүүрийг даван туулахад туслах платформ болно.</p>
                         </div>
                     </div>
                     <div className="flex justify-center px-5 pt-8">
                         <button onClick={handleBtn} className="px-20 py-2 rounded-full text-xl text-[#272343] bg-[#ffd803] font-semibold">Тест өгөх</button>
                         {
                             selectItem && (
-                                <div className="fixed top-0 left-0 pl-10 justify-center items-center w-[100vw] h-[100vh]">
+                                <div className="fixed top-0 left-0 justify-center items-center w-[100vw] h-[100vh]">
                                     <div className="relative flex justify-center items-center w-[100vw] h-[100vh]">
-                                            <button onClick={closeHandle} className="w-[100vw]  h-[100vh] absolute-50 top-0 left-0" >
+                                            <button onClick={closeHandle} className="w-[100vw]  h-[100vh] absolute-50 bg-[black] opacity-50 inset-0" >
                                                 1
                                             </button>
-                                        <div className="bg-slate-600 p-3 rounded-xl absolute z-[1] flex flex-col">
+                                        <div className="bg-[white] p-3 rounded-xl absolute z-[1] flex flex-col">
                                             <div className="flex justify-between text-white gap-3">
-                                                <h1 className="text-2xl text-center pt-3 pl-10">Test өгөх боломжтой сэдвүүд</h1>
-                                                <button type="button" onClick={closeHandle} class="bg-white rounded-md px-1 h-10 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                                <h1 className="text-2xl text-center pt-2 pl-10 text-[#272343]">Тест өгөх боломжтой сэдвүүд</h1>
+                                                <button type="button" onClick={closeHandle} class="rounded-md px-[5px] pl-[] h-10 inline-flex items-center justify-center text-gray-500 pt-3">
                                                 <span class="sr-only">Close menu</span>
                                                 <svg class="h-6 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                                 </svg>
                                                 </button>
                                             </div>
-                                            <div className="flex flex-col gap-5 justify-center pt-15 text-white ">
-                                                <button onClick={()=>changeTest("Сэтгэл гутрал")} href="/taketest" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black  hover:bg-stone-50">Сэтгэл гутрал</button>
-                                                <button onClick={()=>changeTest("Түгшүүр")} href="" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black hover:bg-stone-50">Түгшүүр</button>
-                                                <button onClick={()=>changeTest("Стресс")} href="" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black hover:bg-stone-50">Стресс</button>
-                                                <button onClick={()=>changeTest("Өөртөө итгэх итгэл")} href="" className="border rounded-xl bg-[#D1D5DB] text-black text-center py-2 hover:text-black hover:bg-stone-50">Өөртөө итгэх итгэл</button>
+                                            <div className="flex flex-col gap-5 justify-center pt-10 px-[20px]">
+                                                <button onClick={()=>changeTest("Сэтгэл гутрал")} href="/taketest" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Сэтгэл гутрал</button>
+                                                <button onClick={()=>changeTest("Түгшүүр")} href="" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Түгшүүр</button>
+                                                <button onClick={()=>changeTest("Стресс")} href="" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Стресс</button>
+                                                <button onClick={()=>changeTest("Өөртөө итгэх итгэл")} href="" className=" rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2 ">Өөртөө итгэх итгэл</button>
                                             </div>
-                                            <button onClick={closeHandle} className="text-right pt-13 pr-5 text-white">Хаах</button>
+                                            <button onClick={closeHandle} className="text-right pt-[20px] px-[10px] text-[#2d334a]">Хаах</button>
                                         </div>
                                     </div>
                                 </div>
