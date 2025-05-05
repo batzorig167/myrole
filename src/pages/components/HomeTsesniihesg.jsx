54a67577089bc005cc08127e258b8eed2ab1772d
Bayra <tsbanzai74@gmail.com>
2025-05-05 17:44:06 +0800

    Dasgal HomeTsesniihesg dasgal

--- src/pages/components/HomeTsesniihesg.jsx
+++ src/pages/components/HomeTsesniihesg.jsx
@@ -48,84 +48,10 @@ export default function HomeTsesniihesg() {
             сэтгэл гутралыг эмчлэх, өөрийн буруу зүйлээ ойлгуулахад чиглэнэ.
           </h2>
         </div>
-      </div>
-      {selectItem && (
-        <div className="fixed top-0 left-0 justify-center items-center w-[100vw] h-[100vh]">
-          <div className="relative flex justify-center items-center w-[100vw] h-[100vh]">
-            <button
-              onClick={closeHandle}
-              className="w-[100vw] h-[100vh] absolute-50 bg-[black] opacity-50 top-0 left-0"
-            >
-              1
-            </button>
-            <div className="bg-[white] p-3 rounded-xl absolute z-[1] flex flex-col">
-              <div className="flex justify-between text-white gap-3">
-                <h1 className="text-2xl text-center pt-3 pl-10 text-[#272343]">
-                  Тест өгөх боломжтой сэдвүүд
-                </h1>
-                <button
-                  type="button"
-                  onClick={closeHandle}
-                  className="text-[#272343] pt-[20px] "
-                >
-                  <span class="sr-only">Close menu</span>
-                  <svg
-                    class="h-6 w-7"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
-              </div>
-              <div className="flex flex-col gap-5 justify-center pt-[25px]">
-                <button
-                  onClick={() => changeTest("Сэтгэл гутрал")}
-                  href=""
-                  className="rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2"
-                >
-                  Сэтгэл гутрал
-                </button>
-                <button
-                  onClick={() => changeTest("Түгшүүр")}
-                  href=""
-                  className="rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2"
-                >
-                  Түгшүүр
-                </button>
-                <button
-                  onClick={() => changeTest("Стресс")}
-                  href=""
-                  className="rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2"
-                >
-                  Стресс
-                </button>
-                <button
-                  onClick={() => changeTest("Өөртөө итгэх итгэл")}
-                  href=""
-                  className="rounded-xl bg-[#bae8e8] text-[#2d334a] text-center py-2"
-                >
-                  Өөртөө итгэх итгэл
-                </button>
-              </div>
-              <button
-                onClick={closeHandle}
-                className="text-right pt-[25px] px-[10px] text-[#2d334a]"
-              >
-                Хаах
-              </button>
-            </div>
-          </div>
+        <div className="text-center text-white text-xl">
+          <a href="/dasgal" className="border rounded-[18px] bg-[#6246ea] px-10 py-2">Тайвшруулах дасгал</a>
         </div>
-      )}
+      </div>
     </div>
   );
 }
